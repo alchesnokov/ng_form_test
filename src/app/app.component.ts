43ac7b52ab31881ab21622f4e5ec97a612ec51aa
alchesnokov <thechesnokov@gmail.com>
2018-06-27 03:20:54 +0300

    add some logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  testForm: FormGroup;
+  url: string = '';
+  name: string = '';
+  email: string = '';
+  phone: number;
+  password: string = '';
+  confirmPassword: string = '';
+
+  constructor (formBuilder: FormBuilder) {
+    this.testForm = formBuilder.group(
+      {
+        'url': [null, Validators.required],
+        'name': [null, Validators.required, Validators.pattern(/[А-я][A-z]/)],
+        'email': [null, Validators.required, Validators.email],
+        'phone': [null, Validators.required],
+        'password': [null, Validators.required, Validators.minLength(6)],
+        'confirmPassword': [null, Validators.required, Validators.minLength(6)]
+      }
+    )
+  }
+
+  alert: string = 'Поле заполнено неверно';
 }
