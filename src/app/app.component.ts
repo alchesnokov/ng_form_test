a98100bd386155cecfb74d81d5feb10801baeb3e
alchesnokov <thechesnokov@gmail.com>
2018-06-28 03:30:04 +0300

    fix validators, add json on page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {FormGroup, FormBuilder, Validators, ValidatorFn} from '@angular/forms';
+import {group} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -10,25 +11,32 @@ import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 export class AppComponent {
 
   testForm: FormGroup;
-  url: string = '';
-  name: string = '';
-  email: string = '';
-  phone: number;
-  password: string = '';
-  confirmPassword: string = '';
+  // url: string;
+  // name: string;
+  // email: string;
+  // phone: number;
+  // password: string;
+  // confirmPassword: string;
 
   constructor (formBuilder: FormBuilder) {
     this.testForm = formBuilder.group(
       {
         'url': [null, Validators.required],
-        'name': [null, Validators.required, Validators.pattern(/[А-я][A-z]/)],
-        'email': [null, Validators.required, Validators.email],
-        'phone': [null, Validators.required],
-        'password': [null, Validators.required, Validators.minLength(6)],
-        'confirmPassword': [null, Validators.required, Validators.minLength(6)]
+        'name': [null, [Validators.required, Validators.pattern(/[А-я]/)]],
+        'email': [null, [Validators.required, Validators.email]],
+        'phone': [null, [Validators.required]],
+        'password': [null, [Validators.required, Validators.minLength(6)]],
+        'confirmPassword': [null, [Validators.required, Validators.minLength(6)]]
       }
     )
   }
 
   alert: string = 'Поле заполнено неверно';
+
+  // private passwordEqual() {
+  //   if (this.testForm === group.get('confirmPassword').value) {
+  //     return null
+  //   } else {this.alert}
+  // }
+
 }
